3496d26dc8430f9b44c66efa6e41b82d739ece3e
one <one@example.com>
2022-01-20 08:05:36 +0530

    Adding all the files1

---
+++ src/main/java/jar/App.java
@@ -0,0 +1,13 @@
+package jar;
+
+/**
+ * Hello all this morning!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
